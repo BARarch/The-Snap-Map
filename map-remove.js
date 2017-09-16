003dd8ee0e8839266165c131140104f074dd105c
BARarch <anthonyquivers@gmail.com>
2017-09-16 18:25:40 -0400

    square and toggle functions

--- map-remove.js
+++ map-remove.js
@@ -21,6 +21,8 @@ for (var i = 0; i < classes.length; i++) {
 
 var leftBlock = document.createElement("div");
 var rightBlock = document.createElement("div");
+var scale = document.getElementById(scaleID);
+
 
 leftBlock.style.position = "absolute";
 leftBlock.style.top = "0";
@@ -36,8 +38,15 @@ rightBlock.style.width = "30%"
 rightBlock.style.height = "100%"
 rightBlock.style.backgroundColor = "rgba(9, 17, 27, 0.94)";
 
+scale.style.position = "absolute";
+scale.style.bottom = "22px";
+scale.style.right = "30%";
+scale.style.color = "rgb(235, 235, 235)";
+document.getElementsByClassName("widget-scale-ruler")[0].style.borderColor = "rgb(235, 235, 235)";
+
 document.body.appendChild(leftBlock);
 document.body.appendChild(rightBlock);
+document.body.appendChild(scale);
 
 function square(){
 	var width =  document.body.clientWidth;
@@ -53,6 +62,9 @@ function square(){
 		leftBlock.style.height = "100%";
 		rightBlock.style.width = pct.toString() + "%";
 		rightBlock.style.height = "100%";
+
+		scale.style.bottom = "22px";
+		scale.style.right = pct.toString() + "%";
 	}else{
 		// For Portait framed body widow
 		pct = Math.floor((aspectRatioVert * 100) / 2);
@@ -60,8 +72,36 @@ function square(){
 		leftBlock.style.width = "100%";
 		rightBlock.style.height = pct.toString() + "%";
 		rightBlock.style.width = "100%";
+
+		scale.style.right = "0";
+		scale.style.bottom = pct.toString() + "%";
 	}
 
-	//Get Scale Element and move it too
+}
+
+function toggle(){
+	var state = ""
+	if (document.getElementById(ids[0]).style.display == "none"){
+		// If all markings are off bring everything back, turn off side blocks
+		state = "";
+		square();
+		leftBlock.style.display = "none";
+		rightBlock.style.display = "none";
+	}else{
+		state = "none";
+		leftBlock.style.display = "";
+		rightBlock.style.display = "";
+	}
+
+	for (var i = 0; i < ids.length; i++) {
+  		document.getElementById(ids[i]).style.display = state;
+	}
+
+	for (var i = 0; i < classes.length; i++) {
+	  var div = document.getElementsByClassName(classes[i]);
+	  for (var j = 0; j < div.length; j++) {
+	    div[j].style.display = state;
+	  }
+	}
 
 }
